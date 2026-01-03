5c63e64938699efe70ec7f3267ba3db47d23d6f3
Ony <ony@ony.world>
2026-01-03 19:21:49 +0300

    breaking changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { sveltekitOG } from '@ethercorps/sveltekit-og/plugin';
 import arraybuffer from "vite-plugin-arraybuffer";
+
 export default defineConfig({
 	preview: {
 		allowedHosts: ['maki.tempel-goblin.ts.net']
 	},
-	plugins: [arraybuffer(), tailwindcss(), sveltekit(), sveltekitOG()]
+	plugins: [arraybuffer(), tailwindcss(), sveltekit(), sveltekitOG({})]
 });
