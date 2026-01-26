6623675a801fa4072a1b2f921865265b3f253f9d
Ony <ony@ony.world>
2026-01-26 21:14:07 +0300

    layout update

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -8,20 +8,21 @@
 <ul class="mt-4 list-none space-y-4">
 	{#each data.posts as post}
 		{@const { metadata } = post}
+
 		<li title={metadata.title}>
 			<a
-				href={`/posts/${metadata.slug}`}
-				class="group flex items-baseline justify-between gap-4 no-underline"
+				href="/posts/{metadata.slug}"
+				class="group flex flex-col-reverse gap-2 no-underline sm:flex-row sm:items-baseline sm:gap-4"
 			>
-				<span class="link font-medium max-w-64 truncate">
+				<span class="link font-medium">
 					{metadata.title}
 				</span>
-
 				<hr
-					class="h-px flex-1 border-dotted border-muted transition-colors group-hover:border-fg"
+					class="hidden h-px flex-1 border-dotted border-muted transition-colors group-hover:border-fg sm:block"
 				/>
-
-				<time class="shrink-0 text-muted transition-colors group-hover:text-fg">
+				<time
+					class="shrink-0 text-sm text-muted transition-colors group-hover:text-fg sm:text-base"
+				>
 					{formatDate(metadata.date)}
 				</time>
 			</a>
